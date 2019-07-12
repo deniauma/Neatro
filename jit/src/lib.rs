3a8f6efbba267b736c0a094acc8058c82f290b64
mdeniaud <mathieu.deniaud@crossknowledge.com>
2019-07-12 16:51:26 +0200

    JIT add 2 unit tests

--- jit/src/lib.rs
+++ jit/src/lib.rs
@@ -9,18 +9,28 @@ const PAGE_READWRITE: u32 = 0x04;
 const PAGE_EXECUTE_READ: u32 = 0x20;
 
 pub struct AsmBuf {
-    addr: *mut u8
+    addr: *mut u8,
+    len: isize
 }
 
 impl AsmBuf {
     pub fn new() -> Self {
         let buf = unsafe { VirtualAlloc(ptr::null_mut(), PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE) };
-        let mut old: win32::DWORD = 0;
-        unsafe { VirtualProtect(buf, PAGE_SIZE, PAGE_EXECUTE_READ, &mut old) };
 
         Self {
-            addr: buf as *mut u8
+            addr: buf as *mut u8,
+            len: 0
         }
+    }
+
+    pub fn finalize(&self) {
+        let mut old: win32::DWORD = 0;
+        unsafe { VirtualProtect(self.addr as win32::LPVOID, PAGE_SIZE, PAGE_EXECUTE_READ, &mut old) };
+    }
+
+    pub fn push_instruct_byte(&mut self, byte: u8) {
+        unsafe { self.addr.offset(self.len).write(byte) };
+        self.len += 1;
     }
 }
 
@@ -38,7 +48,35 @@ mod tests {
 
     #[test]
     fn asmbuf_create() {
-        let asmbuf = AsmBuf::new();
+        let mut asmbuf = AsmBuf::new();
         assert_ne!(asmbuf.addr, ptr::null_mut());
+        asmbuf.push_instruct_byte(0x48);
+        // asmbuf.push_instruct_byte(0x89);
+        // asmbuf.push_instruct_byte(0xf8);
+        asmbuf.push_instruct_byte(0xc7);
+        asmbuf.push_instruct_byte(0xc0);
+        asmbuf.push_instruct_byte(0x03);
+        asmbuf.push_instruct_byte(0x00);
+        asmbuf.push_instruct_byte(0x00);
+        asmbuf.push_instruct_byte(0x00);
+        // assert_eq!(asmbuf.len, 3);
+        asmbuf.push_instruct_byte(0xc3);
+        asmbuf.finalize(); //asm: mov rax, 0x03; ret
+        let res = unsafe { core::mem::transmute::<_, fn() -> i32>(asmbuf.addr)() };
+        assert_eq!(res, 3);
+    }
+
+    #[test]
+    fn asmbuf_echo_fn() {
+        let mut asmbuf = AsmBuf::new();
+        assert_ne!(asmbuf.addr, ptr::null_mut());
+        asmbuf.push_instruct_byte(0x48);
+        asmbuf.push_instruct_byte(0x89);
+        asmbuf.push_instruct_byte(0xc8);
+        assert_eq!(asmbuf.len, 3);
+        asmbuf.push_instruct_byte(0xc3);
+        asmbuf.finalize(); //asm: mov rax, rcx; ret
+        let res = unsafe { core::mem::transmute::<_, fn(i32) -> i32>(asmbuf.addr)(3) };
+        assert_eq!(res, 3);
     }
 }
